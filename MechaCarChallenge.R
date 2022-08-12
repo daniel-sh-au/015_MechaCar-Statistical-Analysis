f4333b36b9a6838220e079c063a67c985b8ba066
Daniel Au <daniel.au123@hotmail.com>
2022-08-12 15:02:01 -0400

    completed Deliverable 1

---
+++ MechaCarChallenge.R
@@ -0,0 +1,13 @@
+# *************** Deliverable 1 ***************
+
+# load dplyr package
+library(dplyr)
+
+# import MechaCar_mpg.csv file
+mpg_data <- read.csv('Resources/MechaCar_mpg.csv',check.names = F,stringsAsFactors = F) 
+
+# perform linear regression
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_data)
+
+# determine p-value and r-sqaured value
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_data))
