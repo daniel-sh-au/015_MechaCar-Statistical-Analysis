ec1b119dd11142cdd6988d6538bb63acedf42dc6
Daniel Au <daniel.au123@hotmail.com>
2022-08-14 12:30:35 -0400

    completed Deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -11,3 +11,14 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # determine p-value and r-squared value
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_data))
+
+# *************** Deliverable 2 ***************
+
+# import Suspnsion_Coil.csv file
+coil_data <- read.csv('Resources/Suspension_Coil.csv',check.names = F,stringsAsFactors = F) 
+
+# create total summary dataframe 
+total_summary <- coil_data %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+# create a lot summary dataframe
+lot_summary <- coil_data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
